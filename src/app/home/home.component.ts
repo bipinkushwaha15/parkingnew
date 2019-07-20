786aca91a0a01f91dc456bfba579454b5be6b1fe
bipinkushwaha15 <30337277+bipinkushwaha15@users.noreply.github.com>
2019-07-20 07:28:46 +0530

    Ts file changed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Parking } from '../parking.component';
 import { ActivatedRoute } from '@angular/router';
 
 export type FuzzyScore = number;
- 
+
 export interface FuzzySegment {
-    value: string;
-    isMatch: boolean;
+  value: string;
+  isMatch: boolean;
 }
 
 @Component({
@@ -25,8 +25,8 @@ export class HomeComponent implements OnInit {
       parking_area: "C.V. Raman Nagar",
       parking_locality: "Nagavarapalya Layout",
       parking_city: "Bangalore",
-      parking_state:"Karnataka",
-      parking_country:"India",
+      parking_state: "Karnataka",
+      parking_country: "India",
       parking_pin: 560093
     },
     {
@@ -36,8 +36,8 @@ export class HomeComponent implements OnInit {
       parking_area: "C.V. Raman Nagar",
       parking_locality: "Old Madras Road",
       parking_city: "Bangalore",
-      parking_state:"Karnataka",
-      parking_country:"India",
+      parking_state: "Karnataka",
+      parking_country: "India",
       parking_pin: 560093
     },
     {
@@ -47,8 +47,8 @@ export class HomeComponent implements OnInit {
       parking_area: "Baiyapanhalli",
       parking_locality: "Old Madras Road",
       parking_city: "Bangalore",
-      parking_state:"Karnataka",
-      parking_country:"India",
+      parking_state: "Karnataka",
+      parking_country: "India",
       parking_pin: 560038
     },
     {
@@ -58,29 +58,42 @@ export class HomeComponent implements OnInit {
       parking_area: "Baiyapanhalli",
       parking_locality: "Baiyapanhalli",
       parking_city: "Bangalore",
-      parking_state:"Karnataka",
-      parking_country:"India",
+      parking_state: "Karnataka",
+      parking_country: "India",
       parking_pin: 560038
     },
   ]
   values: string[] = [];
-  final = [];
-  address: string[]=[];
+  final: string[] = [];
+  address: string[] = [];
+  emptyString:boolean;
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
-  
+
   onKeyPress(event) {
-    this.final=this.park;
+    //this.final=this.park;
+    let partLoc =event.target.value;
+    for (let i = 0; i < this.park.length; i++) {
+      this.address[i] = this.park[i].parking_address;
+      //console.log(this.address[i]);
+    }
+    for (let i = 0; i < this.address.length; i++) {
+      //console.log(this.address[i].indexOf(partLoc)+"  "+this.address[i].lastIndexOf(partLoc));
+      console.log(this.address[i].match(partLoc));
+      if (this.address[i].match(partLoc)){
+        this.final[i] = this.address[i];
+      }
+    }
   }
   getParking() {
     console.log("hello");
   }
-  selectParking(address: string) {
-    console.log(address);
+  selectParking(event) {
+    alert(event.string.value);
   }
-  getcity(event){
-    console.log(event.target.value);
+  getcity() {
+    console.log();
   }
 }
